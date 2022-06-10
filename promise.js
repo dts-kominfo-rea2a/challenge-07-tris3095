4f1ad2409ca4e6bc5092bec190bc3d37b96ba2b1
Tris <triskominfoss@gmail.com>
2022-06-10 16:06:10 +0700

    tantangan 07

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (data) => {
+  let tampung = 0;
+  try {
+    let arrIxx = await promiseTheaterIXX();
+    let arrVgc = await promiseTheaterVGC();
+
+    tampung += arrIxx.filter(element => (element.hasil === data)).length;
+    tampung += arrVgc.filter(element => (element.hasil === data)).length;
+    return tampung;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
